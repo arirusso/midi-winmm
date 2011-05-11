e838d6262e75aa6d9440c2413fb09336cba9f7c2
Ari Russo <ari.russo@gmail.com>
2011-05-10 20:47:01 -0400

    my standard win setup

--- test/config.rb
+++ test/config.rb
@@ -6,7 +6,7 @@ module TestHelper::Config
   # before running tests
   
   NumDevices = 4 # this is the total number of MIDI devices that are connected to your system
-  TestInput = Device.first(:input) # this is the device you wish to use to test input
-  TestOutput = Device.last(:output) # likewise for output
+  TestInput = Input.first # this is the device you wish to use to test input
+  TestOutput = Output.all[1] # likewise for output
   
 end
